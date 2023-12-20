2885fd6ce5a25530b2271aa33e0770890f73e5cc
Chief-Enigma <julian.bambauer@hackster.ch>
2023-12-20 17:04:52 +0100

    .

--- digitectoolweb.client/vite.config.js
+++ digitectoolweb.client/vite.config.js
@@ -1,40 +1,16 @@
 import { fileURLToPath, URL } from 'node:url';
-
 import { defineConfig } from 'vite';
 import plugin from '@vitejs/plugin-react';
 import fs from 'fs';
 import path from 'path';
-import child_process from 'child_process';
 
-const baseFolder =
-    process.env.APPDATA !== undefined && process.env.APPDATA !== ''
-        ? `${process.env.APPDATA}/ASP.NET/https`
-        : `${process.env.HOME}/.aspnet/https`;
+const certificateName = "digitectoolweb.client"; // Replace with your actual certificate name
 
-const certificateArg = process.argv.map(arg => arg.match(/--name=(?<value>.+)/i)).filter(Boolean)[0];
-const certificateName = certificateArg ? certificateArg.groups.value : "digitectoolweb.client";
+const certFilePath = path.join(process.env.APPDATA, 'ASP.NET/https', `${certificateName}.pem`);
+const keyFilePath = path.join(process.env.APPDATA, 'ASP.NET/https', `${certificateName}.key`);
 
-if (!certificateName) {
-    console.error('Invalid certificate name. Run this script in the context of an npm/yarn script or pass --name=<<app>> explicitly.')
-    process.exit(-1);
-}
-
-const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
-const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
-
-if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
-    if (0 !== child_process.spawnSync('dotnet', [
-        'dev-certs',
-        'https',
-        '--export-path',
-        certFilePath,
-        '--format',
-        'Pem',
-        '--no-password',
-    ], { stdio: 'inherit', }).status) {
-        throw new Error("Could not create certificate.");
-    }
-}
+// Check if the certificate files exist
+const useHttps = fs.existsSync(certFilePath) && fs.existsSync(keyFilePath);
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -52,9 +28,11 @@ export default defineConfig({
             }
         },
         port: 5173,
-        https: {
-            key: fs.readFileSync(keyFilePath),
-            cert: fs.readFileSync(certFilePath),
-        }
+        https: useHttps
+            ? {
+                key: fs.readFileSync(keyFilePath),
+                cert: fs.readFileSync(certFilePath),
+            }
+            : undefined,
     }
-})
+});
