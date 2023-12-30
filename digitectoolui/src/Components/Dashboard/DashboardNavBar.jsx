cdc07b755cf5afe781f865581876ff34ab958295
Chief-Enigma <julian.bambauer@hackster.ch>
2023-12-30 15:41:23 +0100

    adding icons

--- digitectoolui/src/Components/Dashboard/DashboardNavBar.jsx
+++ digitectoolui/src/Components/Dashboard/DashboardNavBar.jsx
@@ -5,30 +5,41 @@ import "./DashboardNavBar.css"
 export const DashboardNavBar = (props) => {
     return (
         <nav className="DashboardNavBar">
+
             <ul>
-                <li>
+                <li className="DashboardNavElement">
                     <Link to="main">
-                        <a>Today</a>
+                        <span class="material-symbols-outlined">
+                            event
+                        </span><a>Today</a>
                     </Link>
                 </li>
                 <li>
                     <Link to="main">
-                        <a>Schichtplan bearbeiten</a>
+                        <a className="DashboardNavElement"><span class="material-symbols-outlined">
+                            edit_calendar
+                        </span>Schichtplan bearbeiten</a>
                     </Link>
                 </li>
                 <li>
                     <Link to="main">
-                        <a>Mitarbeiter</a>
+                        <a className="DashboardNavElement"><span class="material-symbols-outlined">
+                            groups
+                        </span>Mitarbeiter</a>
                     </Link>
                 </li>
                 <li>
                     <Link to="main">
-                        <a>Tickets</a>
+                        <a className="DashboardNavElement"><span class="material-symbols-outlined">
+                            receipt_long
+                        </span>Tickets</a>
                     </Link>
                 </li>
                 <li>
                     <Link to="main">
-                        <a>Einstellungen</a>
+                        <a className="DashboardNavElement"><span class="material-symbols-outlined">
+                            settings
+                        </span>Einstellungen</a>
                     </Link>
                 </li>
             </ul>
