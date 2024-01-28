c95804c73a29d3c7274d397631c2da50521d5fa9
Chief-Enigma <julian.bambauer@hackster.ch>
2024-01-28 16:23:12 +0100

    y

--- digitectoolui/src/Pages/Dashboard/RequestHolliday/RequestHollidaysComponents/RequestForm/RequestFormMain.jsx
+++ digitectoolui/src/Pages/Dashboard/RequestHolliday/RequestHollidaysComponents/RequestForm/RequestFormMain.jsx
@@ -1,6 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { format } from "date-fns";
 
 export const RequestForm = () => {
+  const user = JSON.parse(localStorage.getItem("user"));
+  const requestLayOut = {
+    id: {},
+    requestNumber: 0,
+    requestType: "RequestHoliDay",
+    requestState: "open",
+    creationDate: format(new Date(), "yyyy-MM-dd"),
+    requestFrom: user.personalnumber,
+    dateFrom: format(Date(), "yyyy-MM-dd"),
+    dateTo: format(Date(), "yyyy-MM-dd"),
+    note: "",
+  };
+  const [request, setRequest] = useState(requestLayOut);
+
+  //* Handle TextEdit Actions *//
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+
+    // Setze dateTo auf das gleiche Datum wie dateFrom, wenn dateFrom geändert wird
+    if (name === "dateFrom") {
+      setRequest((prevData) => ({
+        ...prevData,
+        dateFrom: value,
+        dateTo: value, // Setze dateTo auf das gleiche Datum wie dateFrom
+      }));
+    } else {
+      // Wenn ein anderes Feld als dateFrom geändert wird, aktualisiere nur dieses Feld
+      setRequest((prevData) => ({
+        ...prevData,
+        [name]: value,
+      }));
+    }
+  };
+
+  useEffect(() => {
+    console.log(request);
+  }, [request]);
+
   return (
     <div className="DashboardContendBox">
       <div className="RequestForm">
@@ -24,6 +63,9 @@ export const RequestForm = () => {
                   className="RequestDateInput"
                   type="date"
                   id="RequestHollydayFrom"
+                  name="dateFrom"
+                  value={request.dateFrom}
+                  onChange={handleInputChange}
                 />
               </div>
               <div className="RequestCol-20">
@@ -39,6 +81,9 @@ export const RequestForm = () => {
                   className="RequestDateInput"
                   type="date"
                   id="RequestHollydayTo"
+                  name="dateTo"
+                  value={request.dateTo}
+                  onChange={handleInputChange}
                 />
               </div>
             </div>
@@ -48,6 +93,9 @@ export const RequestForm = () => {
                   className="RequestTextInput"
                   type="text"
                   placeholder="Bemerkung..."
+                  name="note"
+                  value={request.note}
+                  onChange={handleInputChange}
                 />
               </div>
             </div>
